0fa6e41a1c7a7ab780f7b5a1d165428a790973f9
RyuTatsukiSama <dorianribault@gmail.com>
2025-04-06 19:42:22 +0200

    Test gameLauncher failed

--- QTTest/VisualStudio/Aetova/Aetova/main.cpp
+++ QTTest/VisualStudio/Aetova/Aetova/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <QtWidgets/qmainwindow.h>
 #include <QtWidgets/qapplication.h>
+#include "gamelauncher.h"
 
 int main(int argc, char* argv[])
 {
@@ -9,6 +10,9 @@ int main(int argc, char* argv[])
 	QMainWindow window;
 
 	window.show();
+
+	GameLauncher launch;
+	launch.launchGame("Journeep", "Journeep");
 
 	return app.exec();
 }
