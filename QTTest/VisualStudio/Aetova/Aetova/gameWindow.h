e0f7cefbc542e3ba5513908c26fad68f4a3fcc5b
RyuTatsukiSama <dorianribault@gmail.com>
2025-04-14 22:38:36 +0200

    Init work on Widget & PushButton

---
+++ QTTest/VisualStudio/Aetova/Aetova/gameWindow.h
@@ -0,0 +1,27 @@
+#ifndef GAMEWINDOW_H
+#define GAMEWINDOW_H
+
+#include <QtWidgets/qwidget.h>
+
+class GameWindow : public QWidget
+{
+public:
+
+	Q_OBJECT
+
+	GameWindow();
+	~GameWindow();
+
+private:
+
+};
+
+GameWindow::GameWindow()
+{
+}
+
+GameWindow::~GameWindow()
+{
+}
+
+#endif
--- QTTest/VisualStudio/Aetova/Aetova/main.cpp
+++ QTTest/VisualStudio/Aetova/Aetova/main.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 #include <QtWidgets/qmainwindow.h>
 #include <QtWidgets/qapplication.h>
+#include <QtWidgets/qpushbutton.h>
 #include "gamelauncher.h"
+
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 720
 
 int main(int argc, char* argv[])
 {
 	QApplication app(argc,argv);
 
-	QMainWindow window;
-
+	QWidget window;
+	window.resize(SCREEN_WIDTH, SCREEN_HEIGHT);
 	window.show();
+	window.setWindowTitle(QApplication::translate("Test", "Test-en"));
 
-	GameLauncher launch;
-	launch.launchGame("Journeep", "Journeep");
+	QWidget window2;
+	window2.resize(100, 100);
+
+	QPushButton* button = new QPushButton(
+		QApplication::translate("childwidget", "Press me"), &window);
+	button->move(100, 100);
+	button->show();
+
+	QApplication::connect(button, &QPushButton::released, &window2, &QWidget::show);
 
 	return app.exec();
 }
