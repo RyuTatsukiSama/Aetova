0b41be4e963c389b529ffae31b598c064091226e
RyuTatsukiSama <dorianribault@gmail.com>
2025-04-20 16:31:19 +0200

    add Layout

--- QTTest/VisualStudio/Aetova/Aetova/main.cpp
+++ QTTest/VisualStudio/Aetova/Aetova/main.cpp
@@ -4,6 +4,7 @@
 #include <QtWidgets/qpushbutton.h>
 #include <QtWidgets/qlabel.h>
 #include <QtCore/qdir.h>
+#include <QtWidgets/qboxlayout.h>
 #include "gamelauncher.h"
 #include "gamewindow.h"
 #include "common.h"
@@ -17,42 +18,52 @@ int main(int argc, char* argv[])
 
 	QWidget* window = new QWidget();
 	window->resize(SCREEN_WIDTH, SCREEN_HEIGHT);
-	window->show();
 	window->setWindowTitle(QApplication::translate("Aetova", "Aetova"));
 
-	// Splash Art
+	// root layout
+	QVBoxLayout* mainLayout = new QVBoxLayout(window);
 
-	QLabel* imageLabel = new QLabel(window);
+	// Splash Art 
+	// -- TO DO --
+	// - Make it look more clean
 
-	imageLabel->setGeometry(
-		0,
-		0,
-		window->width(),
-		window->height() / 2.f
-	);
-
+	QLabel* imageLabel = new QLabel();
 
 	QPixmap pixmap(":/sprite/wallpaper.png");
-	if (pixmap.isNull())
+	if (pixmap.isNull()) // Do a function like LoadSprite of Pierre to don't have to write the test everytime
 	{
 		std::cout << "Error : Qt didn't load the image at path :/sprite/wallpaper.png" << std::endl;
 	}
+
 	imageLabel->setPixmap(pixmap);
 	imageLabel->setScaledContents(true);
-	imageLabel->show();
+	imageLabel->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+	imageLabel->setMaximumHeight(window->height() / 2);
+	mainLayout->addWidget(imageLabel);
+
+	// Button
+	GameLauncher launcher;
+
+	QPushButton* button = new QPushButton(
+		QApplication::translate("childwidget", "Launch Game"),
+		window
+	);
+
+	button->setFixedSize(150, 40);
+	mainLayout->addWidget(button, 0, Qt::AlignHCenter);
+
+	QApplication::connect(button, &QPushButton::released, [&launcher]() {
+		launcher.launchGame("Journeep", "Journeep");
+		});
+
+	// Bottom Layout for Team & DataSheet
+	QHBoxLayout* bottomLayout = new QHBoxLayout();
 
 	// Team
 
 	QLabel* labelTeam = new QLabel(window);
 
 	labelTeam->setFrameStyle(QFrame::Panel | QFrame::Sunken);
-
-	labelTeam->setGeometry(
-		0,
-		window->height() / 2.f,
-		window->width() / 2.f,
-		window->height() / 2.f
-	);
 
 	QString textTeam = QString::fromUtf8(u8R"(
 	<b><u>Team :</u></b><br><br>
@@ -74,7 +85,8 @@ int main(int argc, char* argv[])
 	labelTeam->setText(textTeam.trimmed());
 
 	labelTeam->setAlignment(Qt::AlignHCenter | Qt::AlignTop);
-	labelTeam->show();
+	labelTeam->setWordWrap(true);
+	bottomLayout->addWidget(labelTeam);
 
 	// Data Sheet
 
@@ -118,29 +130,14 @@ int main(int argc, char* argv[])
 	labelDataSheet->setText(textDS.trimmed());
 
 	labelDataSheet->setAlignment(Qt::AlignHCenter | Qt::AlignTop);
-	labelDataSheet->show();
+	labelDataSheet->setWordWrap(true);
+	bottomLayout->addWidget(labelDataSheet);
 
-	// Button
+	mainLayout->addLayout(bottomLayout);
 
-	GameLauncher launcher;
 
-	QPushButton* button = new QPushButton(
-		QApplication::translate("childwidget", "Launch Game"),
-		window
-	);
-
-	button->setGeometry(
-		window->width() / 2 - button->width() / 2,
-		window->height() / 2 - button->height() / 2,
-		100,
-		50
-	);
-
-	button->show();
-
-	QApplication::connect(button, &QPushButton::released, [&launcher]() {
-		launcher.launchGame("Journeep", "Journeep");
-		});
+	window->setLayout(mainLayout);
+	window->show();
 
 	return app.exec();
 }
