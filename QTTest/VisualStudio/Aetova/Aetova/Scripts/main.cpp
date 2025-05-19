7ac8ffe1aa3914dca844dd024a03286fc59bb280
RyuTatsukiSama <163751364+RyuTatsukiSama@users.noreply.github.com>
2025-05-19 13:49:22 +0200

    move to screen center

--- QTTest/VisualStudio/Aetova/Aetova/Scripts/main.cpp
+++ QTTest/VisualStudio/Aetova/Aetova/Scripts/main.cpp
@@ -3,6 +3,7 @@
 #include "GameProcess/GameThread/gamethread.h"
 #include "QtGui/qfontdatabase.h"
 #include "QtGui/qfont.h"
+#include "QtGui/qscreen.h"
 
 // Use QVBoxLayout for better scaling
 // Qt has a file manager system call QFile
@@ -13,6 +14,13 @@ int main(int argc, char* argv[])
 	GameWindow* gw = new GameWindow();
 	gw->resize(SCREEN_WIDTH, 1000);
 	gw->setWindowTitle(QApplication::translate("Aetova", "Aetova"));
+
+	QScreen* screen = QGuiApplication::primaryScreen();
+	QRect screenGeometry = screen->availableGeometry();
+	int x = (screenGeometry.width() - gw->width()) / 2;
+	int y = (screenGeometry.height() - gw->height()) / 2;
+	gw->move(x, y);
+
 	gw->show();
 
 	return app.exec();
