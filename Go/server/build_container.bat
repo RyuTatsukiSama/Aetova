9cf3d00429bceca9cc0f0680cff17ea037528bf6
RyuTatsukiSama <dorianribault@gmail.com>
2025-11-05 14:34:33 +0100

    docker init

---
+++ Go/server/build_container.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set image_name="docker-gs-ping"
+set container_name="aetova-serv"
+set app_port="8080" # Port that the golang code listen
+set container_port="8080" # Port to access the code
+
+REM Rebuild image
+docker image rm %image_name%
+docker build -t %image_name% .
+
+REM Puis relancer le conteneur
+REM If a container already run
+docker stop %container_name%  
+docker rm %container_name%
+docker run -d --name %container_name% --env-file .env -p 8080:8080 %image_name%
+
+pause
