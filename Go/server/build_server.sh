8eba124b6d989e42d73c80309ed2490e94ea2a26
RyuTatsukiSama <dorianribault@gmail.com>
2025-11-26 11:55:56 +0100

    build docker in bash

---
+++ Go/server/build_server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+image_name="image-aetova-serv"
+container_name="aetova-serv"
+
+# Get the .env port
+while IFS='=' read -r key value; do
+    if [ "$key" = "PORT" ]; then
+        machine_port="$value"
+        container_port="$value"
+    fi
+done < .env
+
+# Rebuild image
+docker image rm "$image_name"
+docker build --build-arg PORT="$container_port" -t "$image_name" .
+
+# Relaunch the container
+# If a container already runs
+docker stop "$container_name"
+docker rm "$container_name"
+docker run -d --name "$container_name" --env-file .env -p "$machine_port:$container_port" "$image_name"
+
+read -p "Press Enter to continue..."
