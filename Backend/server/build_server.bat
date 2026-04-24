31c6d1990aa3950b9926f095629f282c609dddd0
RyuTatsukiSama <dorianribault@gmail.com>
2026-04-24 11:09:23 +0200

    fix build_server.bat

--- Backend/server/build_server.bat
+++ Backend/server/build_server.bat
@@ -19,7 +19,18 @@ docker container rm %container_name%
 
 REM Rebuild image
 docker image rm %image_name%
+
 docker build --build-arg PORT=%container_port% -t %image_name% .
+if %errorlevel% neq 0 (
+	echo Error Build
+	pause
+	exit /b 1
+)
 
 REM Relaunch the container
 docker run -d --name %container_name% --env-file .env -p %machine_port%:%container_port% %image_name%
+if %errorlevel% neq 0 (
+	echo Eroor run
+	pause
+	exit /b 1
+)
