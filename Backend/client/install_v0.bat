b4ce84da06e69842319e7d64fb0ff46b81d7c380
RyuTatsukiSama <dorianribault@gmail.com>
2026-05-31 15:12:06 +0200

    add tool for putting the v1 0 0

---
+++ Backend/client/install_v0.bat
@@ -0,0 +1,30 @@
+@echo off
+
+:: Delete the game if it exist
+if exist ".\Shipyard\app\BuildOranys" (
+    rmdir /s /q ".\Shipyard\app\BuildOranys"
+    if %errorlevel% neq 0 (
+        echo "Error in remove"
+    )   
+)
+
+:: Replace it by an older version
+"C:\Program Files\7-Zip\7z.exe" x ..\server\Shipyard\v1_0_0.zip -o.\Shipyard\app
+if %errorlevel% neq 0 (
+    echo "Error in compresion"
+)
+
+:: Change the version in the app manifest
+
+set FILE=".\Shipyard\AppManifest_0.json"
+
+powershell -Command ^
+  "$j = Get-Content '%FILE%' -Raw | ConvertFrom-Json;" ^
+  "$j.Version = 0;" ^
+  "$j | ConvertTo-Json | Set-Content '%FILE%'"
+if %errorlevel% neq 0 (
+    echo "Error in manifest modif"
+)
+
+pause
+exit
