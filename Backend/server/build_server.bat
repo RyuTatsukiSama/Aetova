5b552f7e51d1c75a78b7ebd0bdb50eb21f488def
RyuTatsukiSama <dorianribault@gmail.com>
2026-06-02 16:33:17 +0200

    docker build script

--- Backend/server/build_server.bat
+++ Backend/server/build_server.bat
@@ -5,14 +5,6 @@ set container_name="aetova-serv"
 set machine_port="51418"
 set container_port="51418"
 
-REM Get the .env port
-REM for /f "tokens=1,2 delims==" %%a in (.env) do (
-REM     if "%%a"=="PORT" (
-REM         set machine_port=%%b
-REM         set container_port=%%b
-REM     )
-REM )
-
 REM Stop the container
 docker stop %container_name%
 docker container rm %container_name%
@@ -28,7 +20,7 @@ if %errorlevel% neq 0 (
 )
 
 REM Relaunch the container
-docker run -d --name %container_name% --env-file .env -p %machine_port%:%container_port% %image_name%
+docker run -d --name %container_name% -p %machine_port%:%container_port% %image_name%
 if %errorlevel% neq 0 (
 	echo Eroor run
 	pause
--- Backend/server/build_server.sh
+++ Backend/server/build_server.sh
@@ -5,14 +5,6 @@ container_name="aetova-serv"
 container_port="51418"
 machine_port="51418"
 
-# Get the .env port
-# while IFS='=' read -r key value; do
-#     if [ "$key" = "PORT" ]; then
-#         machine_port="$value"
-#         container_port="$value"
-#     fi
-# done < .env
-
 # Stop the container
 docker stop "$container_name"
 docker container rm "$container_name"
@@ -22,4 +14,4 @@ docker image rm "$image_name"
 docker build --build-arg PORT="$container_port" -t "$image_name" .
 
 # Relaunch the container
-docker run -d --name "$container_name" --env-file .env -p "$machine_port:$container_port" "$image_name"
+docker run -d --name "$container_name" -p "$machine_port:$container_port" "$image_name"
